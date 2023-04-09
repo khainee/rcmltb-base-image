91e3111faa99060c709d8eda88bd6fbf6ef7a00a
Mr Khaine <118953358+khainee@users.noreply.github.com>
2023-04-09 15:34:27 +0630

    Update a.sh

--- a.sh
+++ a.sh
@@ -4,17 +4,13 @@ echo "███████╗████████╗ █████╗ █
       ╚════██║   ██║   ██╔══██║██╔══██╗   ██║   ██║██║╚██╗██║██║   ██║
       ███████║   ██║   ██║  ██║██║  ██║   ██║   ██║██║ ╚████║╚██████╔╝
       ╚══════╝   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝   ╚═╝╚═╝  ╚═══╝ ╚═════╝"
-apt-get -y -q update && \
-apt-get -y -q upgrade && \
-apt-get install -y -q software-properties-common && \
-add-apt-repository -y -q universe && \
-add-apt-repository -y -q multiverse && \
-add-apt-repository -y -q restricted && \
-apt-get install -y -q python3 python3-pip python3-lxml aria2 \
-qbittorrent-nox tzdata p7zip-full p7zip-rar xz-utils curl wget pv jq \
-ffmpeg locales neofetch git make g++ gcc automake unzip \
-autoconf libtool libsodium-dev libcurl4-openssl-dev libc-ares-dev swig \
-libssl-dev libcrypto++-dev zlib1g-dev libsqlite3-dev libfreeimage-dev
+apt-get -y update && \
+apt-get -y upgrade && \
+apt-get install -y software-properties-common && \
+add-apt-repository -y universe && \
+add-apt-repository -y multiverse && \
+add-apt-repository -y restricted && \
+apt-get install -y python3 python3-pip python3-lxml aria2 qbittorrent-nox tzdata p7zip-full p7zip-rar xz-utils curl wget pv jq ffmpeg locales neofetch git make g++ gcc automake unzip autoconf libtool libsodium-dev libcurl4-openssl-dev libc-ares-dev swig libssl-dev libcrypto++-dev zlib1g-dev libsqlite3-dev libfreeimage-dev
 
 echo "██████╗ ██╗   ██╗███╗   ██╗███╗   ██╗██╗███╗   ██╗ ██████╗ 
 ██╔══██╗██║   ██║████╗  ██║████╗  ██║██║████╗  ██║██╔════╝ 
@@ -27,14 +23,13 @@ echo "██████╗ ██╗   ██╗███╗   ██╗██�
 
 
 export MEGA_SDK_VERSION=4.10.0
-git clone -q https://github.com/meganz/sdk.git --depth=1 -b v${MEGA_SDK_VERSION} /home/sdk \
+git clone https://github.com/meganz/sdk.git --depth=1 -b v${MEGA_SDK_VERSION} /home/sdk \
 && cd /home/sdk && rm -rf .git \
 && autoupdate -fIv && ./autogen.sh \
 && ./configure --disable-silent-rules --enable-python --with-sodium --disable-examples \
 && make -j$(nproc --all) \
 && cd bindings/python/ && python3 setup.py bdist_wheel \
 && cd dist && ls && pip3 install --no-cache-dir megasdk-*.whl 
-echo ""
-curl -s https://rclone.org/install.sh | bash
-locale-gen en_US.UTF-8 -q
+curl  https://rclone.org/install.sh | bash
+locale-gen en_US.UTF-8
 apt-get -y autoremove && apt-get -y autoclean
