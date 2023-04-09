bc78ac0dbd4ab7065bf5b89fc623cb4ddb3266e6
Mr Khaine <118953358+khainee@users.noreply.github.com>
2023-04-09 10:05:03 +0630

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,3 +1,4 @@
+echo "START"
 apt-get -y update && \
 apt-get -y upgrade && \
 apt-get install -y software-properties-common && \
@@ -9,3 +10,11 @@ qbittorrent-nox tzdata p7zip-full p7zip-rar xz-utils curl wget pv jq \
 ffmpeg locales neofetch git make g++ gcc automake unzip \
 autoconf libtool libsodium-dev libcurl4-openssl-dev libc-ares-dev swig \
 libssl-dev libcrypto++-dev zlib1g-dev libsqlite3-dev libfreeimage-dev
+echo "Running"
+git clone https://github.com/meganz/sdk.git --depth=1 -b v$MEGA_SDK_VERSION /home/sdk \
+&& cd /home/sdk && rm -rf .git \
+&& autoupdate -fIv && ./autogen.sh \
+&& ./configure --disable-silent-rules --enable-python --with-sodium --disable-examples \
+&& make -j$(nproc --all) \
+&& cd bindings/python/ && python3 setup.py bdist_wheel \
+&& cd dist && ls && pip3 install --no-cache-dir megasdk-*.whl 
