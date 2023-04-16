edfe2a959247c0a6a76bd8c99349689cf5c91c15
Mr Khaine <118953358+khainee@users.noreply.github.com>
2023-04-16 23:42:02 +0630

    Update a.sh

--- a.sh
+++ a.sh
@@ -10,7 +10,7 @@ apt-get install -y software-properties-common && \
 add-apt-repository -y universe && \
 add-apt-repository -y multiverse && \
 add-apt-repository -y restricted && \
-apt-get install -y python3 python3-pip python3-lxml aria2 tzdata p7zip-full p7zip-rar xz-utils curl wget pv jq ffmpeg locales neofetch git make g++ gcc automake unzip autoconf libtool libsodium-dev libcurl4-openssl-dev libc-ares-dev swig libssl-dev libcrypto++-dev zlib1g-dev libsqlite3-dev libfreeimage-dev
+apt-get install -y --no-install-recommends python3 python3-pip python3-lxml aria2 tzdata p7zip-full p7zip-rar xz-utils curl wget pv jq ffmpeg locales neofetch git make g++ gcc automake unzip autoconf libtool libsodium-dev libcurl4-openssl-dev libc-ares-dev swig libssl-dev libcrypto++-dev zlib1g-dev libsqlite3-dev libfreeimage-dev
 
 echo "██████╗ ██╗   ██╗███╗   ██╗███╗   ██╗██╗███╗   ██╗ ██████╗ 
 ██╔══██╗██║   ██║████╗  ██║████╗  ██║██║████╗  ██║██╔════╝ 
@@ -19,7 +19,7 @@ echo "██████╗ ██╗   ██╗███╗   ██╗██�
 ██║  ██║╚██████╔╝██║ ╚████║██║ ╚████║██║██║ ╚████║╚██████╔╝
 ╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═══╝╚═╝  ╚═══╝╚═╝╚═╝  ╚═══╝ ╚═════╝ 
                                                            "
-export MEGA_SDK_VERSION=4.8.0
+export MEGA_SDK_VERSION=4.10.0
 git clone https://github.com/meganz/sdk.git --depth=1 -b v${MEGA_SDK_VERSION} /home/sdk \
 && cd /home/sdk && rm -rf .git \
 && autoupdate -fIv && ./autogen.sh \
