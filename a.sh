5a74e6478d89a1df6651b4d8680b63d560012eb6
Mr Khaine <118953358+khainee@users.noreply.github.com>
2023-04-16 22:29:34 +0630

    Update a.sh

--- a.sh
+++ a.sh
@@ -7,9 +7,6 @@ echo "███████╗████████╗ █████╗ █
 apt-get -y update && \
 apt-get -y upgrade && \
 apt-get install -y software-properties-common && \
-add-apt-repository -y universe && \
-add-apt-repository -y multiverse && \
-add-apt-repository -y restricted && \
 apt-get install -y python3 python3-pip python3-lxml aria2 tzdata p7zip-full p7zip-rar xz-utils curl wget pv jq ffmpeg locales neofetch git make g++ gcc automake unzip autoconf libtool libsodium-dev libcurl4-openssl-dev libc-ares-dev swig libssl-dev libcrypto++-dev zlib1g-dev libsqlite3-dev libfreeimage-dev
 
 echo "██████╗ ██╗   ██╗███╗   ██╗███╗   ██╗██╗███╗   ██╗ ██████╗ 
