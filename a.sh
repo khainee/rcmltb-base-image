64eb4561f598a3016e8746427e6d77fb747dbc9a
Mr Khaine <118953358+khainee@users.noreply.github.com>
2023-04-17 11:07:19 +0630

    Update a.sh

--- a.sh
+++ a.sh
@@ -7,7 +7,7 @@ echo "███████╗████████╗ █████╗ █
 apt-get -y update && \
 apt-get -y upgrade && \
 apt-get install -y software-properties-common && \
-apt-get install -y --no-install-recommends python3 python3-pip python3-lxml aria2 tzdata p7zip-full p7zip-rar xz-utils curl wget pv jq ffmpeg locales neofetch git make g++ gcc automake unzip autoconf libtool libsodium-dev libcurl4-openssl-dev libc-ares-dev swig libssl-dev libcrypto++-dev zlib1g-dev libsqlite3-dev libfreeimage-dev
+apt-get install -y --no-install-recommends --no-install-suggests python3 python3-pip python3-lxml aria2 tzdata p7zip-full p7zip-rar xz-utils curl wget pv jq ffmpeg locales neofetch git make g++ gcc automake unzip autoconf libtool libsodium-dev libcurl4-openssl-dev libc-ares-dev swig libssl-dev libcrypto++-dev zlib1g-dev libsqlite3-dev libfreeimage-dev
 
 echo "██████╗ ██╗   ██╗███╗   ██╗███╗   ██╗██╗███╗   ██╗ ██████╗ 
 ██╔══██╗██║   ██║████╗  ██║████╗  ██║██║████╗  ██║██╔════╝ 
