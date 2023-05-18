0584decd099e02c7df3ccd229f2df36d425a18df
Mr Khaine <118953358+khainee@users.noreply.github.com>
2023-05-18 21:23:53 +0630

    Update a.sh

--- a.sh
+++ a.sh
@@ -5,7 +5,7 @@ apt-get install -y software-properties-common && \
 add-apt-repository -y universe && \
 add-apt-repository -y multiverse && \
 add-apt-repository -y restricted && \
-apt-get install -y --no-install-recommends python3 python3-pip python3-lxml aria2 tzdata p7zip-full p7zip-rar xz-utils curl wget pv jq ffmpeg locales neofetch git make g++ gcc automake unzip autoconf libtool libsodium-dev libcurl4-openssl-dev libc-ares-dev swig libssl-dev libcrypto++-dev zlib1g-dev libsqlite3-dev libfreeimage-dev
+apt-get install -y python3 python3-pip python3-lxml aria2 tzdata p7zip-full p7zip-rar xz-utils curl wget pv jq ffmpeg locales neofetch git make g++ gcc automake unzip autoconf libtool libsodium-dev libcurl4-openssl-dev libc-ares-dev swig libssl-dev libcrypto++-dev zlib1g-dev libsqlite3-dev libfreeimage-dev
 echo "\n\n\n\n\n\n\n\n"
 export MEGA_SDK_VERSION=4.8.0
 git clone https://github.com/meganz/sdk.git --depth=1 -b v${MEGA_SDK_VERSION} /home/sdk \
